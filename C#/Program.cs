699a3ea338a62a39d5128bec8437f03d8cf68ab1
Harley <51787264+Harleythetech@users.noreply.github.com>
2022-12-12 17:25:19 +0800

    Add files via upload

---
+++ C#/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace project0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("=== Calculator ===");
+            Console.WriteLine("1: Addition");
+            Console.WriteLine("2: Subtraction");
+            Console.WriteLine("3: Multiplication");
+            Console.WriteLine("4: Division");
+            Console.WriteLine("5: Modulo");
+
+            Console.Write("Choose A Number:");
+            int selector = Convert.ToInt32(Console.ReadLine());
+            Console.Write("First Number:");
+            int firstnum = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Second Number:");
+            int secondnum = Convert.ToInt32(Console.ReadLine());
+
+            switch (selector)
+            {
+                case 1:
+                    int ans = firstnum + secondnum;
+                    Console.WriteLine("The Answer is: " + ans);
+                    break;
+
+                case 2:
+                    int ans2= firstnum - secondnum;
+                    Console.WriteLine("The Answer is: " + ans2);
+                    break;
+
+                case 3:
+                    int ans3= firstnum * secondnum;
+                    Console.WriteLine("The Answer is: " + ans3);
+                    break;
+
+                case 4:
+                    int ans4 = firstnum / secondnum;
+                    Console.WriteLine("The Answer is: " + ans4);
+                    break;
+                
+                case 5:
+                    int ans5 = firstnum % secondnum;
+                    Console.WriteLine("The Answer is: " + ans5);
+                    break;
+
+            }
+        }
+    }
+}
---
+++ C#/Winform 12-12-2022/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Winform_12_12_2022
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public static string tb1;
+        public static string tb2;
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            tb1 = textBox1.Text;
+            tb2 = textBox2.Text;
+            if (tb1 == "" || tb2 == "")
+            {
+                MessageBox.Show("You have entered a wrong User/Password", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                textBox1.Clear();
+                textBox2.Clear();
+            }
+            else if(tb1 == "admin" && tb2 == "admin")
+            {
+
+                DialogResult diag = MessageBox.Show("Do you want to login?", "Confirm", MessageBoxButtons.OKCancel, MessageBoxIcon.Question);
+
+                if (diag == DialogResult.OK)
+                {
+                    new Form2().Show();
+                    this.Hide();
+                }
+                else if (diag == DialogResult.Cancel)
+                {
+                    textBox1.Clear();
+                    textBox2.Clear();
+                }
+            }
+            else
+            {
+                MessageBox.Show("You have entered a wrong User/Password", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                textBox1.Clear();
+                textBox2.Clear();
+            }
+        }
+    }
+}
