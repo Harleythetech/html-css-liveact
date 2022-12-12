2f1f5ef4398b58371d7831135ddc081c50cfa0b0
Harley <51787264+Harleythetech@users.noreply.github.com>
2022-12-12 17:20:57 +0800

    Add files via upload

---
+++ C#/windform 5-12-2022/win12-5-2022/Form1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace win12_5_2022
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public static string tb1;
+        public static string tb2;
+        public static string gender;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            radioButton1.Checked = false;
+            radioButton2.Checked = false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            tb1 = textBox1.Text;
+            tb2 = textBox2.Text;
+            String list = "";
+            if (radioButton1.Checked)
+            {
+                list = list + radioButton1.Text;
+            }
+            if (radioButton2.Checked)
+            {
+                list = list + radioButton2.Text;
+            }
+            gender = list;
+
+            //Confirmation
+            if (tb1 == "" || tb2 == "" || radioButton1.Checked == false && radioButton2.Checked == false)
+            {
+                MessageBox.Show("Please enter a value.","Error: Value Undefined", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            else
+            {
+                
+                DialogResult diag = MessageBox.Show("Are you sure?", "User Confirmation", MessageBoxButtons.OKCancel, MessageBoxIcon.Question);
+                
+                if(diag == DialogResult.OK)
+                {
+                    new Form2().Show();
+                    this.Hide();
+                }
+                else if(diag == DialogResult.Cancel)
+                {
+                    textBox1.Clear();
+                    textBox2.Clear();
+                    radioButton1.Checked = false;
+                    radioButton2.Checked = false;
+                }
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            textBox2.Clear();
+            radioButton1.Checked = false;
+            radioButton2.Checked = false;
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ C#/windform 5-12-2022/win12-5-2022/Form2.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace win12_5_2022
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            label2.Text = Form1.tb1;
+            label4.Text = Form1.tb2;
+            label6.Text = Form1.gender;
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            new Form1().Show();
+            this.Hide();
+        }
+    }
+}
